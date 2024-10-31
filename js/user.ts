667fc72c5d2f24937724d5a46e52b47e0156d8e6
David Ruff <david.ruff@live.de>
2024-10-31 21:04:52 +0100

    add preview menu and changed calendar element creation

--- js/user.ts
+++ js/user.ts
@@ -1,3 +1,5 @@
+let sources:JSON;
+
 function make_url(file: string) {
     let base = window.location;
     return new URL((!file.startsWith("/") ? "/" : "") + file, base.href.replace(base.pathname, ""));
@@ -6,18 +8,18 @@ function make_url(file: string) {
 document.addEventListener("DOMContentLoaded", (event) => {
     let calendar_list = document.getElementById('sources-list')!;
     get_json(make_url('/cmd/source')).then(function (json) {
+        sources=json;
         json.forEach((element: any) => {
-            let cal_tile = document.createElement('div');
-            cal_tile.classList.add('calendar');
-            let icon = document.createElement('img');
-            icon.src = '/static/missing.ico';
-            cal_tile.appendChild(icon);
-            cal_tile.appendChild(document.createElement('br'));
-            cal_tile.appendChild(document.createElement('div'));
-            (cal_tile.lastChild as HTMLDivElement).classList.add('name');
-            (cal_tile.lastChild as HTMLDivElement).title=element['path'];
-            (cal_tile.lastChild as HTMLDivElement).innerText = element['name'];
-            calendar_list.appendChild(cal_tile);
+            let icon_src='/static/missing.ico';
+            calendar_list.innerHTML+=`
+                <div class="calendar">
+                    <img src="${icon_src}">
+                    <br>
+                    <h4 class="name" title="${element['path']}">${element['name']}</h4>
+                    <menu>
+                    <li onclick="preview_calendar('${element['path']}');">preview</li>
+                    </menu>
+            `;
         });
     });
     
@@ -33,3 +35,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
 
 });
+
+function add_popup(content:HTMLElement){
+    let popup=document.createElement("div");
+    popup.className="popup";
+    let close_button=document.createElement("button");
+    close_button.innerText="X";
+    close_button.addEventListener("click",(event)=>{
+        document.removeChild((event.target as HTMLElement)!.parentElement!);
+    })
+    popup.appendChild(content);
+}
+
+
+function preview_calendar(path:URL){
+    fetch(path).then((response)=>{
+        if(response.ok)
+            return response.text()
+        else
+            "could not get calendar"
+    }).then((content)=>{
+        let element=document.createElement("p");
+        element.innerText=content||"";
+        add_popup(element);
+    });
+}
