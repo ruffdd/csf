34b65f6ece72b18fd54f24200aab136be3d95858
David Ruff <david.ruff@live.de>
2025-10-19 17:24:51 +0200

    improve node look

--- js/node-editor.ts
+++ js/node-editor.ts
@@ -1,23 +1,25 @@
 import * as nodes from "./nodes.js";
 
-let nodeToAdd: {() : void} | null = null;
 let nodeMenu: HTMLElement;
 let nodeCanvas: HTMLElement;
+let nodeEditor: HTMLElement;
+
 
 function setup(event: Event) {
 
     nodeMenu = document.getElementById('nodes-toolbar')!;
     nodeCanvas = document.getElementById('nodes-canvas')!;
+    nodeEditor = document.getElementById('node-editor')!;
     {
-        let exampleNodeEntry: HTMLLIElement = document.createElement('li');
-        exampleNodeEntry.innerText = "Example Node";
-        exampleNodeEntry.addEventListener('click', (event) => {nodeToAdd = () =>{new nodes.FilterNodes.ExampleNode(nodeCanvas);}});
-        nodeMenu.appendChild(exampleNodeEntry);
+        new nodes.FilterNodes.ExampleNode(nodeMenu, nodes.FilterNodes.State.MENU_ITEM).htmlElement.addEventListener('click', () => {add_node_to_canvas(nodes.FilterNodes.ExampleNode);});
     }
     console.log("Node editor setup complete");
 }
 document.addEventListener("DOMContentLoaded", setup);
 
+function add_node_to_canvas(node: {() : void}) {
+    
+}
 
 function node_canvas_click(event: MouseEvent) {
     if (nodeToAdd) {
