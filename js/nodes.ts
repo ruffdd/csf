e2116d2147b7e8b71336ff64ddbc109395c0c5fc
David Ruff <david.ruff@live.de>
2025-12-03 22:07:22 +0100

    fix drag and drop

--- js/nodes.ts
+++ js/nodes.ts
@@ -10,28 +10,32 @@ export namespace FilterNodes {
         htmlElement: HTMLDivElement;
 		titleElement: HTMLDivElement;
 		contentDiv: HTMLDivElement;
+		state=State.FLOATING;
 		constructor(parent: HTMLElement, name: string, startState: string) {
             if (this.constructor === Node) {
                 throw new Error("Cannot instantiate abstract class Node");
             }
             this.htmlElement = document.createElement('div') as HTMLDivElement;
-			this.htmlElement.draggable=true;			
 			this.titleElement = document.createElement('div') as HTMLDivElement;
 			this.titleElement.classList.add("node-title");
             this.titleElement.innerHTML = `<h2 class="node-name">${name}</h2>`;
-			
+			this.setState('set')
+
 			this.contentDiv = document.createElement('div') as HTMLDivElement;
 			this.contentDiv.classList.add('node-content');	
             
 			this.htmlElement.classList.add('node');
 			this.htmlElement.appendChild(this.titleElement);
-//			this.titleElement.addEventListener onmousedown = this.mouseDown;
+			this.titleElement.addEventListener('mousedown', ()=>{this.mouseDown(this);});
+			this.titleElement.addEventListener('mouseup', ()=>{this.endDrag(this);});
+			this.titleElement.addEventListener('mousemove',e=>this.dragMove(e,this);
 			this.htmlElement.appendChild(this.contentDiv);
             parent.appendChild(this.htmlElement);
 
-        }
+		}
 
         public setState(state: string) {
+			this.state=state;
             this.htmlElement.classList.remove('menu-item', 'floating', 'set');
             switch (state) {
                 case State.MENU_ITEM:
@@ -46,28 +50,31 @@ export namespace FilterNodes {
             }
         }
 
-		public moveBy(x:number,y:number){
-			let left:number = parseFloat(this.htmlElement.style.left);
-			let top:number = parseFloat(this.htmlElement.style.top);
+		public moveBy(x:number,y:number,node:Node){
+			let left:number = parseInt(node.htmlElement.style.left.replace('px',''));
+			let top:number = parseInt(node.htmlElement.style.top.replace('px',''));
+			if(isNaN(left))
+				left=0;
+			if(isNaN(top))
+				top=0;
 			left+=x;
+
 			top+=y;
-			this.htmlElement.style.left=left.toString();
-			this.htmlElement.style.top=left.toString();
+			node.htmlElement.style.left=left.toString()+"px";
+			node.htmlElement.style.top=top.toString()+"px";
 		}
 
-		private mouseDown(e:MouseEvent){
-			this.titleElement.onmousedown=null;
-			document.onmouseup=this.endDrag;
-			document.onmousemove=this.dragMove;
-			this.setState(State.FLOATING);
+		private mouseDown(node:Node){
+			this.setState('floating')
 		}
 
-		private dragMove(e:MouseEvent){
-			this.moveBy(e.offsetX,e.offsetY);
+		private dragMove(e:MouseEvent:node:Node){
+			if(node.state == State.FLOATING)
+				this.moveBy(e.offsetX,e.offsetY,node);
 		}
 
 		private endDrag(e:Event){
-
+			this.setState('set')
 		}
 
 
