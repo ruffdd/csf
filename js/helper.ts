71e92f3c4b5f058d044226a1ccb3221b5f79ed3f
David Ruff <develop@ruffdd.de>
2025-12-19 23:00:39 +0100

    fix drag drop

---
+++ js/helper.ts
@@ -0,0 +1,7 @@
+export function add(a:[number,number],b:[number,number]):[number,number] {
+    return [a[0]+b[0],a[1]+b[1]];
+}
+
+export function sub(a:[number,number],b:[number,number]):[number,number] {
+    return [a[0]-b[0],a[1]-b[1]];
+}
