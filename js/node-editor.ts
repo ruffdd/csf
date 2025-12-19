67e73048ff6eed6965ff3da933ef02af54cc2545
David Ruff <develop@ruffdd.de>
2025-12-19 23:52:41 +0100

    some refactoring

--- js/node-editor.ts
+++ js/node-editor.ts
@@ -1,22 +1,23 @@
-import * as nodes from "./nodes.js";
+namespace FilterNodes {
 
-let nodeMenu: HTMLUListElement;
-let nodeCanvas: HTMLElement;
-let nodeEditor: HTMLElement;
+  let nodeMenu: HTMLUListElement;
+  let nodeCanvas: HTMLElement;
+  let nodeEditor: HTMLElement;
 
 
-function setup(event: Event) {
+  function setup(event: Event) {
 
     nodeMenu = document.getElementById('nodes-toolbar')! as HTMLUListElement;
     nodeCanvas = document.getElementById('nodes-canvas')!;
     nodeEditor = document.getElementById('node-editor')!;
     {
-		let currentElement : HTMLElement;
-		currentElement=	document.createElement('li');
-		currentElement.innerText="Example";
-		currentElement.addEventListener('click', (e) => {new nodes.FilterNodes.ExampleNode(nodeCanvas);}); 
-		nodeMenu.appendChild(currentElement);
+      let currentElement: HTMLElement;
+      currentElement = document.createElement('li');
+      currentElement.innerText = "Example";
+      currentElement.addEventListener('click', (e) => { new FilterNodes.ExampleNode(nodeCanvas); });
+      nodeMenu.appendChild(currentElement);
     }
     console.log("Node editor setup complete");
+  }
+  document.addEventListener("DOMContentLoaded", setup);
 }
-document.addEventListener("DOMContentLoaded", setup);
