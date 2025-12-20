55489bb5f2d98fc4cdf4491e4b088dd7fb64a3bc
David Ruff <develop@ruffdd.de>
2025-12-20 02:52:26 +0100

    add save functionality

--- js/filter-nodes/node-editor.ts
+++ js/filter-nodes/node-editor.ts
@@ -1,29 +1,59 @@
 namespace FilterNodes {
+  export class NodeEditor {
+    private HTMLNodeMenu: HTMLUListElement;
+    private HTMLNodeCanvas: HTMLElement;
+    private HTMLNodeEditor: HTMLElement;
+    private nodes: Node[] = [];
 
-  let nodeMenu: HTMLUListElement;
-  let nodeCanvas: HTMLElement;
-  let nodeEditor: HTMLElement;
-
-
-  function setup(event: Event) {
-
-    nodeMenu = document.getElementById('nodes-toolbar')! as HTMLUListElement;
-    nodeCanvas = document.getElementById('nodes-canvas')!;
-    nodeEditor = document.getElementById('node-editor')!;
-    {
-      let currentElement: HTMLElement;
-      currentElement = document.createElement('li');
-      currentElement.innerText = "Example";
-      currentElement.addEventListener('click', (e) => { new FilterNodes.ExampleNode(nodeCanvas); });
-      nodeMenu.appendChild(currentElement);
+    constructor() {
+      console.log("start creating node editor");
+      this.HTMLNodeMenu = document.getElementById('nodes-toolbar')! as HTMLUListElement;
+      this.HTMLNodeCanvas = document.getElementById('nodes-canvas')!;
+      this.HTMLNodeEditor = document.getElementById('node-editor')!;
+      {
+        let currentElement: HTMLElement;
+        currentElement = document.createElement('button');
+        currentElement.innerText = "Save";
+        currentElement.addEventListener('click', e => { this.save(); });
+        this.HTMLNodeMenu.appendChild(currentElement);
+      }
+      {
+        let currentElement: HTMLElement;
+        currentElement = document.createElement('button');
+        currentElement.innerText = "Example";
+        currentElement.addEventListener('click', e => { listAdd(this.nodes, new FilterNodes.ExampleNode(this.HTMLNodeCanvas)); });
+        this.HTMLNodeMenu.appendChild(currentElement);
+      }
+      {
+        let currentElement: HTMLElement;
+        currentElement = document.createElement('button');
+        currentElement.innerText = "Subscribe";
+        currentElement.addEventListener('click', e => listAdd(this.nodes, new FilterNodes.SubscribeNode(this.HTMLNodeCanvas)));
+        this.HTMLNodeMenu.appendChild(currentElement);
+      }
     }
-    {
-      let currentElement: HTMLElement;
-      currentElement = document.createElement('li');
-      currentElement.innerText = "Subscribe";
-      currentElement.addEventListener('click', e => new FilterNodes.SubscribeNode(nodeCanvas));
-      nodeMenu.appendChild(currentElement);
+
+
+    public save() {
+      let output = {
+        nodes: this.nodes.map((v) => {
+          return {
+            values: v.values.map(v => { return v.getValueString(); })
+          }
+        })
+      }
+      fetch("/save", {
+        method: 'POST',
+        body: JSON.stringify(output)
+      });
     }
   }
-  document.addEventListener("DOMContentLoaded", setup);
+
+  // class Save {
+  //   nodes:Save.NodeSave[]=[]; 
+
+  //   public NodeSave=class {
+
+  //   }
+  // }
 }
--- js/user.ts
+++ js/user.ts
@@ -1,41 +1,38 @@
-
-let sources: JSON;
-function make_url(file: string) {
-    let base = window.location;
-    return new URL((!file.startsWith("/") ? "/" : "") + file, base.href.replace(base.pathname, ""));
-}
-
-
-function setup(event: Event) {
-}
-document.addEventListener("DOMContentLoaded", setup);
+namespace user {
+    function make_url(file: string) {
+        let base = window.location;
+        return new URL((!file.startsWith("/") ? "/" : "") + file, base.href.replace(base.pathname, ""));
+    }
 
 
+    function UserSetup(event: Event) {
+    }
 
-function add_popup(content: HTMLElement) {
-    let popup = document.createElement("div");
-    popup.className = "popup";
-    let close_button = document.createElement("button");
-    close_button.innerText = "X";
-    close_button.addEventListener("click", (event) => {
-        let target = (event.target as HTMLElement)!;
-        target.parentElement?.parentElement?.removeChild(target.parentElement!);
-    })
-    popup.appendChild(close_button);
-    popup.appendChild(content);
-    document.getElementsByTagName('body')[0].appendChild(popup);
-}
+    function add_popup(content: HTMLElement) {
+        let popup = document.createElement("div");
+        popup.className = "popup";
+        let close_button = document.createElement("button");
+        close_button.innerText = "X";
+        close_button.addEventListener("click", (event) => {
+            let target = (event.target as HTMLElement)!;
+            target.parentElement?.parentElement?.removeChild(target.parentElement!);
+        })
+        popup.appendChild(close_button);
+        popup.appendChild(content);
+        document.getElementsByTagName('body')[0].appendChild(popup);
+    }
 
 
-function preview_calendar(path: URL) {
-    fetch(path).then(function (response) {
-        if (response.ok)
-            return response.text()
-        else
-            "could not get calendar"
-    }).then(function (content) {
-        let element = document.createElement("p");
-        element.innerText = content || "";
-        add_popup(element);
-    });
+    function preview_calendar(path: URL) {
+        fetch(path).then(function (response) {
+            if (response.ok)
+                return response.text()
+            else
+                "could not get calendar"
+        }).then(function (content) {
+            let element = document.createElement("p");
+            element.innerText = content || "";
+            add_popup(element);
+        });
+    }
 }
